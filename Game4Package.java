1122bc3217ada82dce4867ce91a721fdfc2b9225
hyojuntak <67631752+hyojuntak@users.noreply.github.com>
2020-09-29 17:45:43 +0900

    Create Game4Package.java

---
+++ Game4Package.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+
+// 패키지는 다른 언어에서의 라이브러리를 말한다. 자바에서 기본적으로 제공되는 패키지도 있고 스스로 만들어쓰는 패키지가 있다. 
+// import java.util.* -> 패키지 내의 모든 클래스들을 사용하겠다
+// 패키지 이름은 디렉토리 경로와 비슷(같다)하다    \ -> .    (리눅스 유닉스 /)
